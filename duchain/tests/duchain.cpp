1c0520cee3abf9786ce6561cafd4d698b2daba41
Miquel Sabaté <mikisabate@gmail.com>
2011-05-06 21:37:31 +0200

    Re-write of the Duchain test, so it can compile.

--- duchain/tests/duchain.cpp
+++ duchain/tests/duchain.cpp
@@ -1,128 +1,72 @@
-/*
-* This file is part of KDevelop
-*
-* Copyright 2010 Alexander Dymo <adymo@kdevelop.org>
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU Library General Public License as
-* published by the Free Software Foundation; either version 2 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public
-* License along with this program; if not, write to the
-* Free Software Foundation, Inc.,
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-*/
-#include "duchain.h"
+/* This file is part of KDevelop
+ *
+ * Copyright 2010 Alexander Dymo <adymo@kdevelop.org>
+ * Copyright 2011 Miquel Sabaté <mikisabate@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 
 #include <QtTest/QtTest>
 
-#include <language/duchain/types/structuretype.h>
-#include <language/duchain/types/functiontype.h>
-#include <language/duchain/types/integraltype.h>
+#include <parser/rubyparser.h>
+#include <duchain/editorintegrator.h>
+#include <duchain/declarationbuilder.h>
+#include <duchain/tests/duchain.h>
 
-#include "parser/parser.h"
-#include "duchain/editorintegrator.h"
-#include "duchain/declarationbuilder.h"
-
-using namespace KDevelop;
 
 QTEST_MAIN(Ruby::TestDUChain)
+using namespace KDevelop;
 
-namespace Ruby {
+namespace Ruby
+{
 
 TestDUChain::TestDUChain()
     :QObject()
 {
+    /* There's nothing to do here */
 }
 
-TopDUContext* TestDUChain::parse(const QString& input)
+TopDUContext * TestDUChain::parse(const QString & input)
 {
-    Parser *parser = new Parser();
-    ProgramAST *programAST = parser->parse(input);
+    RubyParser * parser = new RubyParser;
+    parser->setCurrentDocument(KUrl("internal"));
+    parser->setContents(input.toUtf8());
+    
+    RubyAst * ast = parser->parse();
     EditorIntegrator editor;
     DeclarationBuilder builder;
     builder.setEditor(&editor);
-    TopDUContext* top = builder.build(IndexedString("file:///internal"), programAST);
-    delete programAST;
+    TopDUContext * top = builder.build(IndexedString("file:///internal"), ast->tree);
+    parser->freeAst(ast);
     return top;
 }
 
 void TestDUChain::declareFunction()
 {
-    QString input = "\
-        def foo(a, b)\n\
-        end\n\
-    ";
-
-    TopDUContext* top = parse(input);
-    DUChainReleaser releaseTop(top);
-    DUChainWriteLocker lock(DUChain::lock());
-
-    QCOMPARE(top->childContexts().count(), 1);
-    QCOMPARE(top->localDeclarations().count(), 1);
-
-    Declaration* dec = top->localDeclarations().at(0);
-    QVERIFY(dec);
-    QCOMPARE(dec->context(), top);
-
-    QCOMPARE(dec->toString(), QString(" foo (mixed, mixed)"));
-
-    FunctionType::Ptr ftype = FunctionType::Ptr::dynamicCast(dec->abstractType());
-    QVERIFY(ftype);
-    QCOMPARE(ftype->arguments().length(), 2);
-
-    QCOMPARE(top->childContexts().at(0)->type(), DUContext::Function);
+    /* TODO: Under construction */
 }
 
 void TestDUChain::declareInstanceFunction()
 {
-    QString input = "\
-    class Foo\n\
-        def foo(a, b)\n\
-        end\n\
-    end\n\
-    ";
-
-    TopDUContext* top = parse(input);
-    DUChainReleaser releaseTop(top);
-    DUChainWriteLocker lock(DUChain::lock());
-
-    //check class
-    QCOMPARE(top->childContexts().count(), 1);
-    QCOMPARE(top->localDeclarations().count(), 1);
-
-    Declaration* dec = top->localDeclarations().at(0);
-    QVERIFY(dec);
-    QCOMPARE(dec->context(), top);
-    QCOMPARE(top->childContexts().at(0)->type(), DUContext::Class);
-
-    StructureType::Ptr stype = StructureType::Ptr::dynamicCast(dec->abstractType());
-    QVERIFY(stype);
-
-    //check instance function
-    DUContext *functionContext = top->childContexts().at(0);
-    QVERIFY(functionContext);
-
-    Declaration* funDec = functionContext->localDeclarations().at(0);
-    QVERIFY(funDec);
-    QCOMPARE(funDec->context(), functionContext);
-    QCOMPARE(functionContext->type(), DUContext::Class);
-    QCOMPARE(functionContext->childContexts().at(0)->type(), DUContext::Function);
-
-    QCOMPARE(funDec->toString(), QString(" foo (mixed, mixed)"));
-
-    //check function arguments
-    FunctionType::Ptr ftype = FunctionType::Ptr::dynamicCast(funDec->abstractType());
-    QVERIFY(ftype);
-    QCOMPARE(ftype->arguments().length(), 2);
+    /* TODO: Under construction */
 }
 
-}
+} // End of namespace ruby
+
 
 #include "duchain.moc"
+
--- duchain/tests/duchain.h
+++ duchain/tests/duchain.h
@@ -1,53 +1,64 @@
-/*
-* This file is part of KDevelop
-*
-* Copyright 2010 Alexander Dymo <adymo@kdevelop.org>
-* Copyright 2008 Niko Sams <niko.sams@gmail.com>
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU Library General Public License as
-* published by the Free Software Foundation; either version 2 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public
-* License along with this program; if not, write to the
-* Free Software Foundation, Inc.,
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-*/
+/* This file is part of KDevelop
+ *
+ * Copyright 2008 Niko Sams <niko.sams@gmail.com>
+ * Copyright 2010 Alexander Dymo <adymo@kdevelop.org>
+ * Copyright 2011 Miquel Sabaté <mikisabate@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+
 #ifndef TESTDUCHAIN_H
 #define TESTDUCHAIN_H
+
+/*
+ * WARNING: This test is outdated. It needs a re-write.
+ */
+
 
 #include <language/duchain/duchain.h>
 #include <language/duchain/duchainlock.h>
 
-namespace Ruby {
+
+namespace Ruby
+{
 
 /**
+ * NOTE: Maybe it's used on the future but right now no one uses it.
  * Manage pointer to TopDUContexts and release them properly, even if a test fails
  */
 struct DUChainReleaser {
-    DUChainReleaser(KDevelop::TopDUContext* top): m_top(top) {}
+    DUChainReleaser(KDevelop::TopDUContext * top): m_top(top) {}
     ~DUChainReleaser() {
         KDevelop::DUChainWriteLocker lock(KDevelop::DUChain::lock());
         KDevelop::DUChain::self()->removeDocumentChain(m_top);
     }
-    KDevelop::TopDUContext* m_top;
+    KDevelop::TopDUContext * m_top;
 };
 
 
-class TestDUChain: public QObject {
+class TestDUChain: public QObject
+{
     Q_OBJECT
 
 public:
     TestDUChain();
 
 private:
-    KDevelop::TopDUContext* parse(const QString &input);
+    KDevelop::TopDUContext * parse(const QString & input);
 
 private slots:
     void declareFunction();
@@ -55,6 +66,7 @@ private slots:
 
 };
 
-}
+} // End of namespace Ruby
+
 
 #endif
