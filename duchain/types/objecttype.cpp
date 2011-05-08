1090a72de3444b5b2f6500f8bf9cdaef2b35985a
Miquel Sabaté <mikisabate@gmail.com>
2011-05-08 02:24:48 +0200

    Cleaning objecttype.

--- duchain/types/objecttype.cpp
+++ duchain/types/objecttype.cpp
@@ -1,46 +1,49 @@
-/*
-* This file is part of KDevelop
-*
-* Copyright 2010 Alexander Dymo <adymo@kdevelop.org>
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU Library General Public License as
-* published by the Free Software Foundation; either version 2 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public
-* License along with this program; if not, write to the
-* Free Software Foundation, Inc.,
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-*/
-#include "objecttype.h"
+/* This file is part of KDevelop
+ *
+ * Copyright 2010 Alexander Dymo <adymo@kdevelop.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 
 #include <language/duchain/types/typeregister.h>
+#include <duchain/types/objecttype.h>
+
 
 using namespace KDevelop;
 
-namespace Ruby {
-
+namespace Ruby
+{
 REGISTER_TYPE(ObjectType);
 
-ObjectType::ObjectType()
-    :KDevelop::StructureType(createData<ObjectType>())
+ObjectType::ObjectType() : KDevelop::StructureType(createData<ObjectType>())
 {
+    /* There's nothing to do here */
 }
 
 ObjectType::ObjectType(const ObjectType& rhs)
-    :KDevelop::StructureType(copyData<ObjectType>(*rhs.d_func()))
+    : KDevelop::StructureType(copyData<ObjectType>(*rhs.d_func()))
 {
+    /* There's nothing to do here */
 }
 
 ObjectType::ObjectType(ObjectTypeData& data)
     :KDevelop::StructureType(data)
 {
+    /* There's nothing to do here */
 }
 
 KDevelop::AbstractType* ObjectType::clone() const
@@ -58,4 +61,5 @@ QString ObjectType::toString() const
     return "Object";
 }
 
-}
+} // End of namespace ruby
+
--- duchain/types/objecttype.h
+++ duchain/types/objecttype.h
@@ -1,38 +1,45 @@
-/*
-* This file is part of KDevelop
-*
-* Copyright 2010 Alexander Dymo <adymo@kdevelop.org>
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU Library General Public License as
-* published by the Free Software Foundation; either version 2 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public
-* License along with this program; if not, write to the
-* Free Software Foundation, Inc.,
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-*/
+/* This file is part of KDevelop
+ *
+ * Copyright 2010 Alexander Dymo <adymo@kdevelop.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+
 #ifndef OBJECTTYPE_H
 #define OBJECTTYPE_H
 
-#include "duchain/duchainexport.h"
+
 #include <language/duchain/types/structuretype.h>
+#include <duchain/duchainexport.h>
 
-namespace Ruby {
 
-class KDEVRUBYDUCHAIN_EXPORT ObjectTypeData: public KDevelop::StructureTypeData {
+namespace Ruby
+{
+
+class KDEVRUBYDUCHAIN_EXPORT ObjectTypeData: public KDevelop::StructureTypeData
+{
 public:
     ObjectTypeData(): KDevelop::StructureTypeData() { }
-    ObjectTypeData(const ObjectTypeData& rhs): KDevelop::StructureTypeData(rhs) { }
+    ObjectTypeData(const ObjectTypeData & rhs) : KDevelop::StructureTypeData(rhs) { }
 };
 
-class KDEVRUBYDUCHAIN_EXPORT ObjectType: public KDevelop::StructureType {
+
+class KDEVRUBYDUCHAIN_EXPORT ObjectType: public KDevelop::StructureType
+{
 public:
     typedef TypePtr<ObjectType> Ptr;
 
@@ -58,6 +65,8 @@ protected:
     TYPE_DECLARE_DATA(ObjectType);
 };
 
-}
+} // End of namespace ruby
+
 
 #endif
+
