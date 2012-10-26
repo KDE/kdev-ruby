4ba19d9bebad35427b71373fb361416ca96ed2b0
Alexander Dymo <adymo@kdevelop.org>
2012-10-27 01:11:46 +0300

    Ruby class template can add base class, properly declare methods, declare members (with attr_accessor). Note, that wizard lets you add several base classes, but this templates uses only the first one.

--- file_templates/classes/ruby_basic/class.rb
+++ file_templates/classes/ruby_basic/class.rb
@@ -1,15 +1,21 @@
 {% load kdev_filters %}
-#
 
+#
 {{ license|lines_prepend:"# " }}
-#
 
 
-class {{ name }}
+class {{ name }}{% if base_classes %} < {{ base_classes.0.baseType }}{% endif %}
 
-    {% for method in methods %}
-    def {{ method.name }}
+    {% if members %}
+
+    attr_accessor {% for member in members %}:{{member.name}}{% if not forloop.last %}, {% endif %}{% endfor %}
+    {% endif %}
+
+    {% for function in functions %}
+
+    def {{ function.name }}
     end
+
     {% endfor %}
 
 end
